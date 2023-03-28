765651f3f23fc3057fe076958fd03df676e8e4db
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2023-03-28 23:05:38 +0300

    add todo list + adding form

---
+++ frontend/team5_frontend/src/components/ToDoList.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/require-v-for-key -->
+<template>
+    <div class="todo" v-for="todo in todos">
+      <div> № {{ todo.id }}</div>
+      <div>create date: {{ todo.createDate }}</div>
+      <div>to do: {{ todo.todo }}</div>
+    </div>
+</template>
+
+<script >
+export default {
+    props: {
+        todos: {
+            type: Array,
+            required: true
+        }
+    }
+}
+ 
+</script>
+
+
+<style scoped>
+  .todo {
+      padding: 30px;
+      border: 1px solid skyblue;
+      margin-top: 10px;
+  }
+</style>
+  
