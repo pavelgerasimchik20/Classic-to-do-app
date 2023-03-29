ee55d977cfe24691121b0ed9c8e44c0d5b841b8a
Pavel Gerasimchik <pavelgerasimchik20@gmail.com>
2023-03-29 12:28:28 +0300

    add Login component

---
+++ frontend/team5_frontend/src/components/LoginComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="login">
+    <GoogleLogin :callback="callback"/>
+    <h3>User email: {{ email }}</h3>
+  </div>
+</template>
+
+<script>
+import { decodeCredential } from 'vue3-google-login';
+
+export default {
+  name: 'LoginComponent',
+  data() {
+    return {
+      userData: null,
+      email: ''
+    }
+  },
+  methods: {
+    callback(response) {
+      this.userData = decodeCredential(response.credential);
+      this.email = this.userData.email;
+      console.log(this.userData.email);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
