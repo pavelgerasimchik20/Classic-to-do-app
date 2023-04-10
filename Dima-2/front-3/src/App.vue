8e0efa7e6ec5f851e33afaa308b9a41ec33df81c
Dichell <9792910@gmail.com>
2023-04-10 09:41:22 +0300

    3 attempt w/router in progress.

---
+++ Dima-2/front-3/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-toolbar dark>
+        <v-toolbar-title>yO yO ToDO</v-toolbar-title>
+    </v-toolbar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
