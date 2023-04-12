efa55465347de4924bc62b6480e037e2919d0431
Dichell <9792910@gmail.com>
2023-04-12 18:05:55 +0300

    init

--- Dima-2/front-3/src/App.vue
+++ Dima-2/front-3/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <v-toolbar dark>
-        <v-toolbar-title>yO yO ToDO</v-toolbar-title>
+        <v-toolbar-title class="text-h2 font-weight-black">yO-yO ToDo</v-toolbar-title>
+        <router-link to="/">Home</router-link> | 
+        <router-link to="/about">About</router-link>
+        <router-link to="/auth">About</router-link>
     </v-toolbar>
     <v-main>
       <router-view/>
---
+++ Dima-2/front-3/src/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    // token: "",
+    // tasks: [],
+    // newTask: ""
+  },
+  getters: {
+  },
+  mutations: {
+    // CHECK_TOKEN(state){
+    //     if(localStorage.getItem("token")){
+    //         state.token = localStorage.getItem("token")
+    //     }
+    // },
+    // LOGIN(state){
+    // },
+    // LOGOUT(state){
+    // },
+
+    // ADD_TODO(state){
+    //     state.tasks.push(
+    //         {
+    //             task: state.newTask,
+    //         }
+    //     )
+    // },
+    // EDIT_TODO(state, todo){
+    // }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
